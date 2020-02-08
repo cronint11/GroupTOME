4dee1d56e649fc5dfae3a6d57e192332c5995115
Carlos Mazon <57258681+reithal@users.noreply.github.com>
2020-02-08 16:51:54 -0500

    Update brewery.js Seperate js file for brewery api

--- assets/scripts/brewery.js
+++ assets/scripts/brewery.js
@@ -1,8 +1,44 @@
-//var breweryAPIKey = "ffbad6db782cf1ad01d5a956d37228a8";
-var breweryAPIURL = "https://api.openbrewerydb.org/breweries";
 
-//var fixCORSErrorURL = "https://cors-anywhere.herokuapp.com/";
+// Hard coding city for now.
+var cityName = "miami";
 
-var nameofbeer = $('#nameOfBeer').text();
+// This will be the ID's for the input.
+// var cityName = $('#citySearch').val();
 
-var beerURL ="https://sandbox-api.brewerydb.com/v2/beer/" + nameofbeer + "/?key=ffbad6db782cf1ad01d5a956d37228a8";
+
+
+var breweryObjects ;
+
+
+function randomByCity (city) {
+  var cityName = city;
+  var beerByCityURL = "https://api.openbrewerydb.org/breweries?by_city=" + cityName;
+
+  makeAjaxRequest(beerByCityURL, renderResults );
+
+};
+
+
+function renderResults (result) {
+
+  breweryObjects = result;
+  for (i = 0; i < breweryObjects.length; i++) {
+    
+      var p = $('<p>');
+      var link = $('<a>').attr("href", breweryObjects[i].website_url);
+      $(link).text("Name: " + breweryObjects[i].name);
+      $(p).append(link);
+      $('body').append(p);
+    }
+ 
+  consolelogResults(result)
+}
+
+
+// DEBUG FUNCTION TO CONSOLE LOG ANYTHING
+function consolelogResults (result) {
+  
+  console.log(result);
+};
+
+randomByCity("miami");
