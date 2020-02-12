910cddc2b6dc0c2c2773cdb3f25b59cae8135d27
Carlos Mazon <57258681+reithal@users.noreply.github.com>
2020-02-12 13:11:16 -0500

    Modified table results to include an edit Modal button

--- assets/scripts/brewery.js
+++ assets/scripts/brewery.js
@@ -2,15 +2,11 @@
 //var cityName = "miami";
 var breweryObjects ;
 
-
-
-
 function randomByCity (city) {
   var cityName = city;
   var beerByCityURL = "https://api.openbrewerydb.org/breweries?by_city=" + cityName;
   makeAjaxRequest(beerByCityURL, renderResults );
 };
-
 
 // REFER TO https://api.eventful.com/docs/events/search
 function beerEvents (city) {
@@ -19,20 +15,54 @@ function beerEvents (city) {
   makeAjaxRequest(eventFulURL, consolelogResults );
 }
 
-
 function renderResults (result) {
   // store the results globally to be used elsewhere and local storage if needed.
   breweryObjects = result;
+  $('tbody').empty();
   for (i = 0; i < breweryObjects.length; i++) {
     
-      var tableRow = $("<tr>");
-      tableRow.append('<td style="width: 10%; text-align: center" class="col"><button class="detailBtn Btn" type="button" data-toggle="modal" data-index=' + [i] + ' data-target="#breweryDetail"><i class="fas fa-info-circle"></i></button><button class="editBtn Btn d-flex" type="button" data-toggle="modal" data-index=' + [i] + '><i class="far fa-edit"></i></button></td>');
+      var editBtn = $("<a>").attr({
+        class:"editBtn Btn d-inline text-muted",
+        href:"#",
+        type:"button",
+        'data-toggle': "modal",
+        'data-target': "#breweryDetail"
+        });
+      editBtn.data({
+          'index': i
+        });
+      var editIcon = $('<i>').addClass("far fa-edit p-1");
+      editBtn.append(editIcon);
+      
+      var infoBtn = $("<a>").attr({
+        class:"detailBtn Btn d-inline text-muted",
+        href:"#",
+        type:"button",
+        'data-toggle': "modal",
+        'data-target': "#breweryDetail"
+        });
+      var infoIcon = $('<i>').addClass("fas fa-info-circle");
+
+      infoBtn.data({
+          'index': i
+        });
+      infoBtn.append(infoIcon);
+
+      var buttonCol = $('<td>').attr({
+        style:"width:20%; text-align: center",
+        class:"col align-center "
+      });
+      buttonCol.append(infoBtn, editBtn)
+      var tableRow = $("<tr>").attr("data-row", i);
+      tableRow.addClass("breweryRow");
+ 
+      tableRow.append(buttonCol);
 
       tableRow.append('<td style="width: 50%; text-align: left" class="col">' + breweryObjects[i].name + '</td>');
       var phone = formatPhone(breweryObjects[i].phone);
       tableRow.append('<td style="width: 40%; text-align: center" class="col">' + phone + '</td>');
       $('tbody').append(tableRow);
-      beerEvents("miami")
+      //beerEvents("miami")
     }
   // DEBUG   
   consolelogResults(result)
@@ -54,9 +84,10 @@ function writeLocal (brewID, brewObject) {
   localStorage.setItem("Brewery-" + brewID , JSON.stringify(brewObject));
 };
 
+
 $(document).on("click", ".detailBtn", function(){
-  var index = $(this).attr("data-index");  
- 
+  var index = $(this).data("index");  
+
   $('.modal-body').empty();
   $('#breweryDetailLongTitle').text(breweryObjects[index].name);
   $('.modal-body').append(breweryObjects[index].street);
@@ -68,9 +99,35 @@ $(document).on("click", ".detailBtn", function(){
   }
 });
 
+$(document).on("click", ".editBtn", function(){
+
+  var index = $(this).data("index"); 
+  var brewNotes = $('<textarea>').attr({
+    class: "",
+    id: "brewNotesText",
+    placeholder: "Tasting Notes/Would Come Back",
+    rows:"5",
+    cols:"50",
+    style:"border:solid 1px"
+  });
+  
+
+  $('.modal-body').empty();
+  $('#breweryDetailLongTitle').text(breweryObjects[index].name);
+  $('.modal-body').append("<span class='align-top'>Beer / Brewery Review:</span>");
+  $('.modal-body').append(brewNotes);
+
+
+  // Call map function 
+
+});
+
 $('#thirstyBtn').on("click", function(){
 // This will be the ID's for the input.
 var cityName = $('#searchCity').val();
+if(!cityName){
+  cityName = "Miami";
+}
 randomByCity(cityName);
 })
 
