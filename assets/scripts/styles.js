44dad47689087375a1db5acab41c28821071b9ad
Orelvis Cardenas <orelviscardenas@gmail.com>
2020-02-15 13:20:59 -0500

    added new page HTML and new files

---
+++ assets/scripts/styles.js
@@ -0,0 +1,10 @@
+// var slider = document.getElementById("slider");
+// var output = document.getElementById("slider");
+// output.innerHTML = slider.value;
+
+// slider.oninput = function() {
+//   output.innerHTML = this.value;
+// }
+
+var myJson= $.getJSON("../../beerNotes");
+console.log(myJson);
